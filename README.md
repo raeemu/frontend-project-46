6eccc3b0c7d7f2f7dab13494ec31a6481f25bca9
raeemu <suerbay55@gmail.com>
2024-05-24 11:33:44 +0300

    4th step done

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/raeemu/frontend-project-46/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/raeemu/frontend-project-46/actions)
+
+### STEP 4 ASCIINEMA: 
+https://asciinema.org/a/FlqfZNcR4ePbMsb1VGMinwMNb
--- index.js
+++ index.js
@@ -1,14 +1,38 @@
 import fs from 'fs';
 import path from 'node:path';
+import _ from 'lodash';
 
-const buildFullPath = (filepath) => path.resolve(process.cwd(), filepath);
+const buildFullPath = (filepath) => path.resolve(process.cwd(), filepath)
 
-const genDiff = (path1, path2) => {
+const gendiff = (path1, path2) => {
     const absolute1 = buildFullPath(path1);
-    const aboslute2 = buildFullPath(path2);
-    const data1 = fs.readFileSync(absolute1, 'utf-8');
-    const data2 = fs.readFileSync(aboslute2, 'utf-8');
-    console.log(data1, data2);
-};
+    const absolute2 = buildFullPath(path2);
+    const data1 = JSON.parse(fs.readFileSync(absolute1, 'utf-8'));
+    const data2 = JSON.parse(fs.readFileSync(absolute2, 'utf-8'));
 
-export default genDiff;
+    const keys1 = Object.keys(data1);
+    const keys2 = Object.keys(data2);
+    const sortedKeys = _.union(keys1, keys2).sort();
+
+    const diff = sortedKeys.reduce((acc, key) => {
+        if (Object.hasOwn(data1, key) && !Object.hasOwn(data2, key)) {
+            acc.push(` - ${key}: ${data1[key]}`)
+        } else if (!Object.hasOwn(data1, key) && Object.hasOwn(data2, key)) {
+            acc.push(` + ${key}: ${data2[key]}`)
+        } else if (Object.hasOwn(data1, key) && Object.hasOwn(data2, key)) {
+            if (data1[key] === data2[key]) {
+                acc.push(`   ${key}: ${data2[key]}`)
+            } else if (data1[key] !== data2[key]) {
+                acc.push(` - ${key}: ${data1[key]}`)
+                acc.push(` + ${key}: ${data2[key]}`)
+            }
+        }
+        return acc;
+    }, ['{'])
+    diff.push('}');
+    const result = diff.join('\n');
+    console.log(result);
+}
+
+export default gendiff;
+
